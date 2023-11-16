e63f982662dceff46daf95e63249aa0513f5d2ce
dashaun <dashaun@dashaun.com>
2023-11-15 22:25:34 -0600

    snapshot

--- src/main/java/io/dashaun/service/youtubeanalysisassistant/Application.java
+++ src/main/java/io/dashaun/service/youtubeanalysisassistant/Application.java
@@ -1,7 +1,19 @@
 package io.dashaun.service.youtubeanalysisassistant;
 
+import org.springframework.ai.client.AiClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +22,64 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+
+@Configuration
+class Config{
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+}
+
+@Component
+class YaaClient {
+
+	private final RestTemplate restTemplate;
+	private final AiClient aiClient;
+
+	YaaClient(RestTemplate restTemplate, AiClient aiClient) {
+        this.restTemplate = restTemplate;
+        this.aiClient = aiClient;
+    }
+
+
+    String downloadTranscriptForUrl(URL url) {
+		return null;
+	}
+	
+	String createPrompt(String transcript) {
+		return null;
+	}
+	
+	URL generateThumbnail(String thumbnailDesign){
+		return null;
+	}
+
+	private static final String API_URL = "https://api.openai.com/v1/images/generations";
+	private static final String OPENAI_API_KEY = "your_api_key_here"; // Replace with actual API key
+
+	String generateImage(String prompt) {
+		var headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("Authorization", "Bearer " + OPENAI_API_KEY);
+
+		var request = new ImageGenerationRequest("dall-e-3",prompt, 1, "1024x1024");
+		var entity = new HttpEntity<>(request, headers);
+		var response = restTemplate.postForEntity(API_URL, entity, String.class);
+		return response.getBody();
+	}
+}
+//public class ImageGenerationRequest {
+//	private String model;
+//	private String prompt;
+//	private int n;
+//	private String size;
+//
+//	// Constructors, getters and setters
+//}
+
+//1024x1024, 1024x1792 or 1792x1024 
+record ImageGenerationRequest(String model, String prompt, int n, String size) {
+	
 }
---
+++ src/test/java/io/dashaun/service/youtubeanalysisassistant/YaaClientTest.java
@@ -0,0 +1,17 @@
+package io.dashaun.service.youtubeanalysisassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class YaaClientTest {
+
+    @Test
+    void name(@Autowired YaaClient yaaClient) {
+        var url = yaaClient.generateThumbnail("Thumbnail Design: A vibrant background with code snippets and the Java coffee cup logo. The text \"Mastering Java 21 & Spring\" in bold, modern font. Icons representing different IDEs like IntelliJ IDEA and VS Code. A 'play' button symbol conveying that the blog post contains a video tutorial.");
+        System.out.println(url);
+    }
+}
